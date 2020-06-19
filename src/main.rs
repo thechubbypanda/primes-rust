414dcf9cdd0d9d3f450e4c0d643716c020b61f82
Keval <keval@thechubbypanda.net>
2020-06-19 23:05:40 +0100

    fix args

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::time::SystemTime;
 use std::env;
 
 fn main() {
-	let end = 10_000_000;
+	let end = 10_000;
 	let mut primes = vec![true; end];
 	primes[0] = false;
 	primes[1] = false;
@@ -19,7 +19,7 @@ fn main() {
 
 	// Print primes if "print" arg passed
 	let args: Vec<String> = env::args().collect();
-	if args[0].eq(&String::from("print")) {
+	if args.len() > 1 && args[1].eq(&String::from("print")) {
 		for n in 0..primes.len() {
 			if primes[n] {
 				println!("{}", n);
